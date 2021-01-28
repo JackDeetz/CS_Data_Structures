e6e2c0183d5c290577cb5d97edaa8b6a5e9ee742
Jack Utter <78005286+JackDeetz@users.noreply.github.com>
2021-01-28 02:09:41 -0800

    Add files via upload

---
+++ FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package Lab_1_FizzBuzz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test() {
+		FizzBuzz.PrintFizzBuzz();
+	}
+
+}
